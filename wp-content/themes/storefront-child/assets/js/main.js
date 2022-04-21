fae6ae38ce41d935dc21420caaa2540628eae503
/usr/bin/vim <support@beget.ru>
2022-04-21 10:34:55 +0300

    fix header & up btn

--- wp-content/themes/storefront-child/assets/js/main.js
+++ wp-content/themes/storefront-child/assets/js/main.js
@@ -57,9 +57,8 @@ if ($(window).width() < 1500) {
 });
 
 // fix header
-/*$(function() {
+$(function() {
     let header = $('.header');
-    
     $(window).scroll(function() {
         if($(this).scrollTop() > 350) {
         header.addClass('header_fixed');
@@ -67,7 +66,7 @@ if ($(window).width() < 1500) {
         header.removeClass('header_fixed');
         }
     });
-});*/
+});
 
 // скрыем блоки для адаптива
 $(function () {
@@ -109,4 +108,20 @@ $(function () {
     if (fndSkrutka.length > 0) {
         $('.product-left #wpgs-gallery .wcgs-carousel').addClass('skrutka');
     } 
+});
+
+//кнопка наверх
+$(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100) {
+            $('.scroll-top-container ').show();
+        } else {
+            $('.scroll-top-container ').hide();
+        }
+    });
+ 
+    $('.scroll-top-container ').click(function(){
+        $('html, body').animate({scrollTop: 0}, 100);
+        return false;
+    });
 });
